67801519a289e8247b79084652a5f0100caaee7e
nihalmohd <nihalmohd828@gmail.com>
2023-10-29 15:18:36 +0530

    User login page responsive issue is fixed :

--- src/Components/SignIn/LoginFormImage.tsx
+++ src/Components/SignIn/LoginFormImage.tsx
@@ -4,20 +4,22 @@ import LoginAuthButtons from './LoginAuthButtons'
 
 const LoginFormImage = () => {
   return (
-    <div className="w-screen h-screen overflow-x-hidden">
-      <div className="flex flex-col justify-center">
-        <div className="w-full h-20 sm:h-14 flex justify-between border shadow-lg border-b " >
+    <div className=" w-full h-screen overflow-x-hidden ">
+      <div className="flex flex-col justify-center ">
+        <div className="w-full h-20 sm:h-14 flex justify-between border shadow-lg border-b bg-white " >
           <img className=" h-full " src="\Images\Untitled-1-01.png" alt="" />
         </div>
-        <div className="w-full h-full flex " >
-          <div className="hidden md:flex w-1/2 justify-center items-center ">
+      </div>
+      <div className="w-full h-full bg-vilot-200 flex p-2">
+        <div className=" sm:hidden md:w-1/2 md:h-full md:flex  md:justify-center md:items-center " >
+          <div className="md:flex w-full h-full justify-center items-center  ">
             <img src="\Images\441-removebg-preview.png" alt="" />
           </div>
-          <div className="w-full sm:w-1/2 flex justify-center items-center">
+        </div>
+          <div className="sm:w-full md:w-1/2  flex justify-center items-center ">
             <LoginAuthButtons />
 
           </div>
-        </div>
       </div>
     </div>
   )
